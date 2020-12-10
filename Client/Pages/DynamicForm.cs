b1a1e451158f2d9d478774253eb4b92c6416010f
Mohsen KOKABI <Mohsen.KOKABI@bambora.com>
2020-12-10 13:18:40 +1100

    Making the DynamicForm.cs partial of DynamicForm.razor.

---
+++ Client/Pages/DynamicForm.cs
@@ -0,0 +1,23 @@
+﻿using Hosted.Shared;
+using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Hosted.Client.Pages
+{
+    public partial class DynamicForm
+    {
+        Form form;
+
+        [Inject]
+        protected HttpClient Http { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var st = await Http.GetStringAsync("Form");
+
+            form = JsonConvert.DeserializeObject<Form>(st, settings: new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto });
+        }
+    }
+}
